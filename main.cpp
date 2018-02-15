eb34deb0b4313c61cb6ccf31866f32a6da620b11
Rytisgit <prytisx@gmail.com>
2018-02-15 11:25:27 +0200

    Pagerinta versija

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include "randutils.hpp"
 #include <iostream>
-#include <iomanip>
-#include <math.h>
-#include <algorithm>
-#include <cfloat>
-#include <stdlib.h>
-#include <random>
-
 const auto iveskite = "Iveskite skaiciu tarp 1 ir 10";
 const auto baigti = " - Baigti Ivesti\n";
-const int end = {-1};
+const int exitnumber = {-1};
 
 inline int median(std::vector<int> medi)
 {
@@ -23,7 +16,6 @@ inline int median(std::vector<int> medi)
 
     return tmedian;
 }
-
 inline double average(const std::vector<int> & temps)
 {
     double sum{0};
@@ -32,37 +24,45 @@ inline double average(const std::vector<int> & temps)
 
     return sum / temps.size();
 }
-inline int getInput(const int min, const int max,const int exit = INT_MAX){
+inline int getInput(const int min, const int max,const int exit = INT_MAX)
+{
     int input{};
-do
+    do
     {
         std::cin>>input;
-         if(input<min||input>max||std::cin.fail()){
+        if(input<min||input>max||std::cin.fail())
+        {
             std::cout<<iveskite;
             if(exit!=INT_MAX)std::cout<<" "<<exit<<baigti;
             else std::cout<<"\n";
             std::cin.clear();
-             // discard 'bad' character(s)
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');}
-            else break;
+            // discard 'bad' character(s)
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else break;
     }
-     while (input!=exit);
-     return input;
+    while (input!=exit);
+    return input;
 
 }
-inline char aORb(){
+inline char aORb()
+{
     char choice{};
-do
+    do
     {
         std::cin>>choice;
-        if(choice!='a'&&choice!='b'){std::cout<<"Rinkites a arba b\n"; std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');}
+        if(choice!='a'&&choice!='b')
+        {
+            std::cout<<"Rinkites a arba b\n";
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
     }
     while(choice!='a'&&choice!='b');
     return choice;
 }
-inline void grade (const std::string vardas,const std::string pavarde,const double pazymiaiapdoroti,const int egzaminas,const char choice){
-    std::cout<<"help";
- if(choice =='b')
+inline void grade (const std::string vardas,const std::string pavarde,const double pazymiaiapdoroti,const int egzaminas,const char choice)
+{   system("cls");
+    if(choice =='b')
     {
         double galutinisPazymis = {pazymiaiapdoroti*0.4+egzaminas*0.6};
         std::cout<<vardas<<" "<<pavarde<<"\nGalutinis pazymis su medianu:"<<std::endl<<galutinisPazymis;
@@ -81,13 +81,12 @@ int main()
     std::cout<<"Irasykite pavarde:";
     std::cin>>pavarde;
     std::cout<<"Pasirinkimai: a - Irasyti Pazymius, b - Atsitiktinai Sugeneruoti Pazymius \n";
-    char choice{aORb()};
     std::vector<int> pazymiai{};
     double egzaminas{};
-    if(choice =='b')  //random pazymiai
+    if(aORb() =='b')  //random pazymiai
     {
         std::cout<<"Irasykite pazymiu kieki:";
-        int n={getInput(1,100)};
+        int n= {getInput(1,100)};
         system("cls");
         //Better random from http://www.pcg-random.org/posts/ease-of-use-without-loss-of-power.html
         randutils::mt19937_rng rng;
@@ -95,30 +94,30 @@ int main()
         {
             pazymiai.push_back(rng.uniform(1,10));
             std::cout<<pazymiai[i]<<"\n";
-
         }
         egzaminas = {rng.uniform(1,10)};
         std::cout<<"egzaminas:"<<egzaminas<<"\n";
     }
-    if(choice =='a')  // irasyti pazymius
+    else  // irasyti pazymius
     {
         int n{};
-        std::cout<<iveskite<<" "<<end<<baigti;
+        std::cout<<iveskite<<" "<<exitnumber<<baigti;
         do
-    {
-        n={getInput(1,10,-1)};
-        if(n == -1){std::cout<<"baigta rasyti pazymius\n";}
-        else pazymiai.push_back(n);
-    }
-     while (n!=-1);
-     std::cout<<"Irasykite Egzamino ivertinima\n";
-         egzaminas={getInput(1,10)};
+        {
+            n= {getInput(1,10,-1)};
+            if(n == -1)
+            {
+                std::cout<<"baigta rasyti pazymius\n";
+            }
+            else pazymiai.push_back(n);
+        }
+        while (n!=-1);
+        std::cout<<"Irasykite Egzamino ivertinima\n";
+        egzaminas= {getInput(1,10)};
     }
     std::cout<<"Pasirinkimai: a - Vidurkis, b - Medianas \n";
-    choice = {aORb()};
-    system("cls");
+    char choice = {aORb()};
     if(choice =='a')grade(vardas,pavarde,average(pazymiai),egzaminas,choice);
-        else grade(vardas,pavarde,(double)median(pazymiai),egzaminas,choice);
-
+    else grade(vardas,pavarde,(double)median(pazymiai),egzaminas,choice);
     return 0;
 }
